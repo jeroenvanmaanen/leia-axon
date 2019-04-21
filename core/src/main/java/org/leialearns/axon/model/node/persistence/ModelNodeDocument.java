f1a53cfa4908ac12f154666b72381bdb4fa9260c
Jeroen van Maanen <jeroen@leialearns.org>
2019-04-21 11:18:51 +0200

    Added index on "data.key" in ModelNodeDocument.

--- core/src/main/java/org/leialearns/axon/model/node/persistence/ModelNodeDocument.java
+++ core/src/main/java/org/leialearns/axon/model/node/persistence/ModelNodeDocument.java
@@ -4,11 +4,16 @@ import lombok.Builder;
 import lombok.Value;
 import org.leialearns.model.ModelNodeData;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "model-node")
 @Value
 @Builder
+@CompoundIndexes(
+    @CompoundIndex(name = "key", def = "{ 'data.key' : 1 }")
+)
 public class ModelNodeDocument {
 
     @Id
