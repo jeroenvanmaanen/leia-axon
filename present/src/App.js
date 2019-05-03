d1e5d7f60a8550e51b60f1649e623a6d82d1d3d6
Jeroen van Maanen <jeroen@leialearns.org>
2019-05-03 08:18:18 +0200

    Fixed URL of Open API logo.

--- present/src/App.js
+++ present/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="header">LEIA learns &#187; Model Structure</div>
         <a href='/swagger-ui.html'>
-          <div class='OpenApiLink'><img src="http://localhost:3000/webjars/springfox-swagger-ui/favicon-32x32.png?v=2.9.2" alt="Open API" /> Open API</div>
+          <div class='OpenApiLink'><img src="/webjars/springfox-swagger-ui/favicon-32x32.png?v=2.9.2" alt="Open API" /> Open API</div>
         </a>
         <Structure />
       </div>
